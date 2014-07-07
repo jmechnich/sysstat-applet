fa60727d39776e06510cb2da94bf3a31bc00a2f7
jmechnich <joerg.mechnich@gmail.com>
2014-07-07 21:17:14 +0200

    Initial revision

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sysstat-applet',
+      version='1.0',
+      description='',
+      author='Joerg Mechnich',
+      author_email='joerg.mechnich@gmail.com',
+      url='https://github.com/jmechnich/sysstat-applet',
+      packages=['sysstatapplet'],
+      scripts=['sysstat-applet']
+     )
