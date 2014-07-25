a410c8c6e705864083f80ed6d259955de5c5451d
jmechnich <joerg.mechnich@gmail.com>
2014-07-25 13:00:33 +0200

    make splash visible on all desktops

--- sysstatapplet/splash.py
+++ sysstatapplet/splash.py
@@ -2,6 +2,8 @@ from util import *
 
 from PyQt4.Qt import *
 
+import subprocess
+
 class Splash(QWidget):
     def __init__(self,sti):
         QWidget.__init__(self)
@@ -10,3 +12,10 @@ class Splash(QWidget):
         self.setWindowOpacity(0.75)
         self.sti = sti
         self.font = QFont("Dejavu Sans", 8)
+    
+    def show(self):
+        QWidget.show(self)
+        subprocess.call( ['xprop', '-id', "0x%x" % self.winId(),
+                          '-f', '_NET_WM_DESKTOP', '32c',
+                          '-set', '_NET_WM_DESKTOP', '0xFFFFFFFF'] )
+        
