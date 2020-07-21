1c7bd64f465b9c96e5efe300432af73f5dd84644
jmechnich <joerg.mechnich@gmail.com>
2020-07-21 21:33:39 +0200

    updated license to GPLv3

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+import setuptools
 
-setup(name='sysstat-applet',
-      version='1.0',
-      description='',
-      author='Joerg Mechnich',
-      author_email='joerg.mechnich@gmail.com',
-      url='https://github.com/jmechnich/sysstat-applet',
-      packages=['sysstatapplet'],
-      scripts=['sysstat-applet']
-     )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='sysstat-applet',
+    version='1.0.0',
+    author='Joerg Mechnich',
+    author_email='joerg.mechnich@gmail.com',
+    description='Tray applet for system monitoring',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jmechnich/sysstat-applet',
+    packages=['sysstatapplet'],
+    install_requires=["appletlib"],
+    scripts=['sysstat-applet'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: X11 Applications :: Qt",
+    ],
+    python_requires='>=3.6',
+)
